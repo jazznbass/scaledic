864ad4c031920efe73471e28e1d073954fbd89a3
Jürgen Wilbert <jazznbass98@gmail.com>
2019-08-04 09:38:13 +0200

    combine short and long labels

--- R/list_scales.R
+++ R/list_scales.R
@@ -6,13 +6,7 @@
 #' @return A list with scales on diferent levels
 #' @export
 list_scales <- function(data, labels = FALSE) {
-  # out <- list()
-  # out$scales <- unique(unlist(lapply(data, function(x) dic_attr(x, .opt$scale))))
-  # out$subscales  <- unique(unlist(lapply(data, function(x)
-  #  paste0(dic_attr(x, .opt$scale), "_", dic_attr(x, .opt$subscale)))))[-1]
-  # out$subscales2 <- unique(unlist(lapply(data, function(x)
-  #  paste0(dic_attr(x, .opt$scale), "_", dic_attr(x, .opt$subscale), "_", dic_attr(x, .opt$subscale_2)))))[-1]
-  # out
+
   filter <- which(sapply(data, function(x) !is.null(attr(x, .opt$dic))))
 
   if(!labels) {
@@ -22,18 +16,28 @@ list_scales <- function(data, labels = FALSE) {
         dic_attr(x, .opt$subscale),
         dic_attr(x, .opt$subscale_2)
       ))
+    out <- as.data.frame(unique(t(out)))
+    names(out) <- c("Scale", "Subscale", "Subscale_2")
   }
+
   if(labels) {
     out <- sapply(data[filter], function(x)
       cbind(
+        dic_attr(x, .opt$scale),
+        dic_attr(x, .opt$subscale),
+        dic_attr(x, .opt$subscale_2),
         dic_attr(x, .opt$scale_label),
         dic_attr(x, .opt$subscale_label),
         dic_attr(x, .opt$subscale_2_label)
       ))
+    out <- as.data.frame(unique(t(out)))
+    names(out) <- c("Scale", "Subscale", "Subscale_2", "Label scale", "Label subscale", "Label subscale_2")
+
   }
-  out <- as.data.frame(unique(t(out)))
-  names(out) <- c("Scale", "Subscale", "Subscale_2")
-  out <- out[order(out[["Scale"]], out[["Subscale"]], out[["Subscale_2"]]), ]
-  rownames(out) <- NULL
-  out
+  #out <- out[order(out[["Scale"]], out[["Subscale"]], out[["Subscale_2"]]), ]
+  #rownames(out) <- NULL
+  out %>%
+    as_tibble() %>%
+    arrange(Scale, Subscale, Subscale_2)
+
 }
