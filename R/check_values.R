3a1050c08b2f708876248d54eff7d951b695f6dd
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2019-08-07 14:46:33 +0200

    added include_missing argument

--- R/check_values.R
+++ R/check_values.R
@@ -5,18 +5,22 @@
 #' @param data A data frame
 #' @param replace Value which relaces unvalid values (e.g., NA).
 #' @param return If TRUE, a data frame is returned with replaced values.
+#' @param include_missing If TRUE, missing values (provided in the dic file)
+#' will be considered as valid values.
 #'
 #' @return A data frame with replaced values if replaces is not NULL.
 #' @export
 #' @examples
 #' dat <- apply_dic(ITRF, dic_ITRF)
 #' check_values(dat, return = FALSE)
-check_values <- function(data, replace = NULL, return = TRUE) {
+check_values <- function(data, replace = NULL, return = TRUE, include_missing = TRUE) {
   id <- which(sapply(data, function(x) !is.null(attr(x, .opt$dic))))
   name <- names(data)
   errors <- list()
   for (i in id) {
     values <- dic_attr(data[[i]], .opt$values)
+    missing <- dic_attr(data[[i]], .opt$missing)
+    if (include_missing) values <- c(values, missing)
     type <- dic_attr(data[[i]], .opt$type)
 
     ### strict checking integers otherwise use float
