f719de7b4c19b11e0fdfea9cb62ae2a3e1ca6408
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2019-08-07 14:47:17 +0200

    initial

---
+++ R/ITRF.R
@@ -0,0 +1,13 @@
+#' Integrated Teacher Report Formula for more than 5000 subjects
+#'
+#' A dataset containing the response teacher formula data (40 items).
+#'
+#' @format A data frame with 6643 rows and 40 variables
+"ITRF"
+
+#' Dictionary file for the Integrated Teacher Report Formula.
+#'
+#' A dictionary file of the German version of the ntegrated Teacher Report Formula (ITRF).
+#'
+#' @format A data frame with 39 rows and 16 variables
+"dic_ITRF"
