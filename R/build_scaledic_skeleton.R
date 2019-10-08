81d5411e4f5c77eefb7701936823b1914185655d
Jürgen Wilbert <jazznbass98@gmail.com>
2019-10-08 10:01:01 +0200

    new

---
+++ R/build_scaledic_skeleton.R
@@ -0,0 +1,15 @@
+#' Build a dictionary file template
+#'
+#' @param filename Character string. Default is 'dic_template.xlsx'
+#'
+#' @return Writes an Excel file with an empty template of a dic file.
+#' @export
+
+build_scaledic_skeleton <- function(filename = "dic_template.xlsx") {
+  out <- matrix(NA, ncol = length(.dic_file) - 1)
+  out <- as.data.frame(out)
+  names(out) <- unlist(.dic_file[-1])
+  openxlsx::write.xlsx(out, filename)
+  cat(filename, " written at ", getwd(), "\n")
+}
+
