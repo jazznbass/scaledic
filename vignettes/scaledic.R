236bf83fb08984b0a5ed9034e4a72eb614585bd1
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2020-02-17 12:36:49 +0100

    update

--- vignettes/scaledic.R
+++ vignettes/scaledic.R
@@ -31,11 +31,11 @@ out <- tribble(
   ~Parameter, ~Meaning, ~Example,
   "label", "A short item label", "itrf_1",
   "scale", "Abreviation of the scale the item belongs to", "irtf",
-  "sub_scale", "Abrevation of the sub scale", "int",
-  "sub_scale_2", "Abrevation of the second order sub scale", "shy",
+  "subscale", "Abrevation of the sub scale", "int",
+  "subscale_2", "Abrevation of the second order sub scale", "shy",
   "scale_label", "Name of the scale", "Integrated Teacher Report Form",
-  "sub_scale_label", "Name of the sub scale", "internalizing problems",
-  "scale_2_label", "Name of the second order sub scale", "shyness",
+  "subscale_label", "Name of the sub scale", "internalizing problems",
+  "subscale_2_label", "Name of the second order sub scale", "shyness",
   "item", "Full text of the item", "Vermeidet die Teilnahme an Diskussionen im Unterricht",
   "index", "An index number", "1",
   "values", "Valid response values in an R manner", "1:5 (for integers 1 to 5) 1,2,3 (for integers 1, 2, 3)",
@@ -62,63 +62,4 @@ list_scales(dat, labels = TRUE, n_items = TRUE) %>%
 
 ## -----------------------------------------------------------------------------
 dat <- check_values(dat, replace = NA)
-
-## ----message=FALSE, warning=FALSE---------------------------------------------
-# Imputation for items of the subscale ITRF_Ext
-dat <- impute_missing(dat, subscale = "Ext")
-
-# Imputation for items of the subscale ITRF_Int
-dat <- impute_missing(dat, subscale = "Int")
-
-
-## -----------------------------------------------------------------------------
-dat %>% 
-  select_scale(subscale = "Int") %>%
-  psych::describe(fast = TRUE) %>%
-  kable(digits = 1)
-
-## -----------------------------------------------------------------------------
-dat %>% 
-  select_scale(subscale = "Int") %>%
-  names2item() %>%
-  psych::describe(fast = TRUE) %>%
-  kable(digits = 1)
-
-## -----------------------------------------------------------------------------
-dat %>%
-  select_scale(scale = "ITRF") %>%
-  names2item(chars = 70, prefix = c("reverse", "subscale", "subscale2")) %>% 
-  sjPlot::sjt.fa(nmbr.fctr = 4, wrap.labels = 70, show.comm = TRUE)
-
-## -----------------------------------------------------------------------------
-dat %>%
-  select_scale(scale = "ITRF", subscale = "Ext", subscale_2 = "APD") %>%
-  names2item(chars = 70) %>% 
-  sjPlot::sjt.itemanalysis(factor.groups.titles = "Academic Productivity/ Disorganization")
-
-dat %>%
-  select_scale(scale = "ITRF", subscale = "Ext", subscale_2 = "OPP") %>%
-  names2item(chars = 70) %>% 
-  sjPlot::sjt.itemanalysis(factor.groups.titles = "Opposotional/ Disruptive")
-
-dat %>%
-  select_scale(scale = "ITRF", subscale = "Int", subscale_2 = "SW") %>%
-  names2item(chars = 70) %>% 
-  sjPlot::sjt.itemanalysis(factor.groups.titles = "Socialy Withdrawn")
-
-dat %>%
-  select_scale(scale = "ITRF", subscale = "Int", subscale_2 = "AD") %>%
-  names2item(chars = 70) %>% 
-  sjPlot::sjt.itemanalysis(factor.groups.titles = "Anxious/ Depressed")
-
-
-## -----------------------------------------------------------------------------
-dat$itrf_ext <- score_scale(dat, scale = "ITRF", subscale = "Ext")
-dat$itrf_int <- score_scale(dat, scale = "ITRF", subscale = "Int")
-
-## -----------------------------------------------------------------------------
-dat %>%
-  select_scores() %>%
-  psych::describe(fast = TRUE) %>%
-  kable(digits = 1)
 
