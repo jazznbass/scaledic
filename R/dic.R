132ce0e06fbb965996d12f74dccecd7610f055a6
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2020-02-17 12:33:57 +0100

    renmove attributes label and labels when printing

--- R/dic.R
+++ R/dic.R
@@ -5,7 +5,6 @@
 #' @return Prints content of varable with dic information added.
 #' @export
 dic <- function(data) {
-  #slice <- slice[slice <= length(data)]
   cat(paste0(dic_attr(data, .opt$item_label), " (", dic_attr(data, .opt$scale_label), ")"), "\n")
   values <- dic_attr(data, .opt$values)
   labels <- names(dic_attr(data, .opt$values))
@@ -13,5 +12,7 @@ dic <- function(data) {
 
   class(data) <- class(data)[!class(data) %in% .opt$dic]
   attr(data, .opt$dic) <- NULL
+  attr(data, "label") <- NULL
+  attr(data, "labels") <- NULL
   print(data)
 }
