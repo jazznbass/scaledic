93fac37c3270b6e796dca5739747654fbf134fb1
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2021-01-12 17:34:34 +0100

    update_homepage

--- R/extract_dic.R
+++ R/extract_dic.R
@@ -62,35 +62,3 @@ extract_dic <- function(data) {
   #out <- out[order(out[[.opt$item_name]]),]
   out
 }
-
-extract_dic_old <- function(data) {
-
-  opt.attr <- .dic_file[
-    !names(.dic_file) %in% c("variable", "values", "value_labels")
-    ]
-  opt.attr <- c(opt.attr, list(class = "class"))
-  dic_names <- names(opt.attr)
-  id <- .get_dic_items(data)
-  N <- length(id)
-  vars <- c(unlist(.dic_file[!names(.dic_file) %in% "variable"]), class = "class")
-  out <- matrix(NA, nrow = N, ncol = length(vars))
-  out <- as.data.frame(out)
-  names(out) <- vars
-
-  for (i in 1:N) {
-    dic <- attr(data[[id[i]]], .opt$dic)
-    for (j in 1:length(opt.attr)) {
-      out[i, opt.attr[[j]]] <- dic[[dic_names[j]]]
-    }
-    x <- paste0(dic$values, ",", collapse = "")
-    x <- substring(x, 1, nchar(x) - 1)
-    out[i, .dic_file[["values"]]] <- x
-    x <- paste0(dic$values, " = ", names(dic$values), "; ", collapse = "")
-    x <- substring(x, 1, nchar(x) - 2)
-    out[i, .dic_file[["value_labels"]]] <- x
-  }
-
-
-
-  out
-}
