e8fdb610124015ed21c6d76fc5e1535afd071887
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2021-04-02 09:03:53 +0200

    renamed example data

--- R/scaledic_data.R
+++ R/scaledic_data.R
@@ -1,6 +1,15 @@
-#' Data ITRF
+#' Example dataset: Integrated Teacher Report Form
 #'
-#' Data of the ITRF and a correponding dic file'
-#' @aliases ex_itrf dic_ITRF
+#' A dataset containing the response teacher form data (40 items).
+#'
+#' @format A data frame with 4776 rows and 40 variables
 #' @docType data
-"ITRF"
+"ex_itrf"
+
+#' Example dictionary file for the Integrated Teacher Report Form.
+#'
+#' A dictionary file of the German version of the integrated Teacher Report Form (ITRF).
+#'
+#' @format A data frame with 39 rows and 15 variables
+#' @docType data
+"dic_ITRF"
