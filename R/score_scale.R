827f154ad3d234da651593a38772b1c33e93d304
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2021-09-10 15:46:22 +0200

    weighted mean

--- R/score_scale.R
+++ R/score_scale.R
@@ -11,22 +11,21 @@
 #' @param max_na Maximum number of NAs that are allowed before returning NA.
 #' A value between 0 and 1 indicates a proportion of values (e.g., 0.5 = 50 percent NAs are allowed).
 #' @param label A character string with a label for the resulting score variable. Automatically generated if label is not set.
-#' @param FUN A function for caluclating the score (e.g., median)
+#' @param FUN A function for calculating the score (e.g., median)
 #' @param ... Further arguments passed to the FUN function (e.g., na.rm = TRUE)
 #' @return A data frame
 #' @export
 
 score_scale <- function(data, filter,
                         scale = NULL, subscale = NULL, subscale_2 = NULL,
-                        bind = FALSE,  sum = FALSE, min_valid = 1, max_na = NA, label = NULL,
+                        bind = FALSE,  sum = FALSE, min_valid = 1, max_na = NA,
+                        label = NULL,
                         FUN = NULL, ...) {
 
   filter <- deparse(substitute(filter))
   if (!is.null(scale) || !is.null(subscale) || !is.null(subscale_2)) {
     filter <- .to_filter(scale = scale, subscale = subscale, subscale_2 = subscale_2)
   }
-
-
 
   .score_scale(data = data, filter = filter, sum = sum, min_valid = min_valid, max_na = max_na, label = label, FUN = FUN, bind = bind, ...)
 
@@ -47,7 +46,7 @@ score_scale <- function(data, filter,
       function_name <- "sum"
     }
     if (!sum) {
-      FUN <- .mean
+      FUN <- .weighted_mean
       args <- list(min_valid = min_valid, max_na = max_na)
       function_name <- "mean"
     }
@@ -58,12 +57,11 @@ score_scale <- function(data, filter,
   df <- data %>% select(all_of(vars))
 
   # Check if all variables are numeric
-  .tmp <- map(df, ~"numeric" %in% class(.))
-  .tmp <- unlist(.tmp)
-  if( !all(.tmp) ) {
+  .tmp <- map(df, ~any(c("numeric", "integer") %in% class(.))) %>% unlist()
+
+  if (!all(.tmp)) {
     stop("Not all variables are numeric.")
   }
-
 
   weight <- df %>%
     map(~ dic_attr(.x, .opt$weight)) %>%
@@ -81,12 +79,19 @@ score_scale <- function(data, filter,
     unlist() %>%
     as.numeric()
 
-
-
-
   df <- apply(df, 1, function(x) {
-    score <- ifelse(sign == 1, x * weight, (max_values - x + min_values) * weight)
-    score <- do.call(FUN, c(list(score), args))
+
+    score <- ifelse(sign == 1, x, max_values - x + min_values)
+
+    if (function_name == "sum") {
+      score <- score * weight
+      score <- do.call(FUN, c(list(score), args))
+    }
+
+    if (function_name == "mean") {
+      score <- do.call(FUN, c(list(score, weight), args))
+    }
+
     score
   })
 
@@ -100,12 +105,21 @@ score_scale <- function(data, filter,
   }
 
   ### set dictionary attributes
-  if( is.null(label)) label <- "score"
+  if (is.null(label)) label <- "score"
 
   attr(df, .opt$dic) <- list()
   dic_attr(df, .opt$class) <- "score"
   dic_attr(df, .opt$score_filter) <- filter
   dic_attr(df, .opt$score_function) <- function_name
+  dic_attr(df, .opt$type) <- "numeric"
+
+  if (function_name == "sum")
+    dic_attr(df, .opt$values) <- paste0(sum(min_values * weight), ":", sum(max_values * weight))
+
+  if (function_name == "mean")
+    dic_attr(df, .opt$values) <- paste0(mean(min_values), ":", mean(max_values))
+
+
   dic_attr(df, .opt$scale) <- NA
   dic_attr(df, .opt$subscale) <- NA
   dic_attr(df, .opt$subscale_2) <- NA
