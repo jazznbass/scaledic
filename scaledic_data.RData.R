a4911d042fd8cde822c1d981e386e0296471adcf
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2021-11-05 10:55:36 +0100

    moved

--- scaledic_data.RData.R
+++
@@ -1,7 +0,0 @@
-
-dic_itrf <- openxlsx::read.xlsx("dic_itrf.xlsx")
-dat_itrf <- openxlsx::read.xlsx("itrf.xlsx")
-ex_itrf <- apply_dic(dat_itrf, dic_itrf)
-save(ex_itrf, dic_itrf, dat_itrf, file = "data/scaledic.RData", compress = "xz")
-
-
