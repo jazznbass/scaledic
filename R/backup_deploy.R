13bc1c917aa3f86f5015ccf408ce05980219ea78
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2023-03-19 11:20:47 +0100

    0.2.3

---
+++ R/backup_deploy.R
@@ -0,0 +1,29 @@
+#' Deploy dictionary information
+#'
+#' @param data A data frame
+#' @param dic_list A list created by the backup_dic() function.
+#'
+#' @return A data frame with added dic information from dic_list
+#' @export
+
+deploy_dic <- function(data, dic_list) {
+  for(item in names(dic_list)) {
+    if(item %in% names(data)) attr(data[[item]], .opt$dic) <- dic_list[[item]]
+  }
+  data <- dic_haven(data)
+  data
+}
+
+
+#' Backup dictionary information
+#'
+#' @param data A data frame containing dic infromation.
+#'
+#' @return A list with dic information. This can later be writen back to a data frame with the deploy_dic() function.
+#' @export
+
+backup_dic <- function(data) {
+  id <- .get_dic_items(data)
+  out <- lapply(data[, id], function(x) attr(x, .opt$dic))
+  out
+}
