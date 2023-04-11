45dd5b3a26dfbc5a180aa1d213028214bb45fec3
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2023-04-11 15:18:29 +0200

    upd

--- vignettes/build_dic_file.R
+++ vignettes/build_dic_file.R
@@ -6,91 +6,96 @@ library(psych)
 library(scaledic)
 knitr::opts_chunk$set(
   collapse = TRUE,
-  comment = "#>"
+  comment = ""
 )
-
-## ----tab_dic_param, echo = FALSE----------------------------------------------
-out <- tribble(
-  ~Parameter, ~Meaning, ~Example,
-  "item_name", "A short item label", "itrf_1",
-  "scale", "Abreviation of the scale the item belongs to", "irtf",
-  "subscale", "Abrevation of the sub scale", "int",
-  "subscale_2", "Abrevation of the second order sub scale", "AD",
-  "item_label", "Full text of the item", "Complains of headaches or stomach aches",
-  "scale_label", "Name of the scale", "Integrated Teacher Report Form",
-  "subscale_label", "Name of the sub scale", "internalizing problems",
-  "subscale_2_label", "Name of the second order sub scale", "Anxious/Depressed",
-  "values", "Valid response values in an R manner", "1:5 (for integers 1 to 5) 1,2,3 (for integers 1, 2, 3)",
-  "value_labels", "Labels for each response value", "0 = not problematic; 1 = slightly problematic; 2 = problematic; 3 = strongly problematic",
-  "missing", "Missing values", "-888, -999",
-  "type", "Data type (factor, integer, float, real, character)", "integer",
-  "weight", "Reversion of item and its weight", "1 (positive), -1 (reverse), 1.5 (positive, weights 1.5 times)",
-  "source", "Reference", "Volpe et al. (2019)",
-  "note", "Further notes", "Item has low descrimination"
-)
-kable(out, caption = "Columns of a dictionary file")
-
-
-## ----echo = FALSE-------------------------------------------------------------
-set.seed(21)
-n <- 20
-n_scale <- 5
-scales <- c("rel", "sui")
-n_var <- n_scale * length(scales)
-dat <- cbind(
-  matrix(sample(1:6, n_scale * n, replace = TRUE), ncol = n_scale),
-  matrix(sample(0:4, n_scale * n, replace = TRUE), ncol = n_scale)
-)
-dat <- as.data.frame(dat)
-names(dat) <- paste(rep(scales, each = n_scale), rep(1:n_scale, length(scales)), sep = "_")
-dat$gender <- sample(c("m", "f", "d"), n, replace = TRUE)
-dat$age <- sample(6:10, n, replace = TRUE)
 
 ## ----echo=FALSE---------------------------------------------------------------
-dat %>% slice(1:10) %>% kable(caption = "First ten rows of the data frame")#rmarkdown::paged_table()
+ex_scaledic_data %>% kable(caption = "ex_scaledic_data")
 
 ## ----echo=FALSE---------------------------------------------------------------
-item_names <- c("How often do you attend church or other religious meetings?",
-"How often do you spend time in private religious activities, such as prayer, meditation or Bible study?", "In my life, I experience the presence of the Divine (i.e., God)", "My religious beliefs are what really lie behind my whole approach to life", "I try hard to carry my religion over into all other dealings in life",
-paste("Did you feel", c("tense", "blue", "irritated", "inferior"), "in the last week?"), "Did you have problems falling asleep in the last week?")
-
-value_labels <- c("1 = Never; 2 = Once a year or less; 3 = A few times a year; 4 = A few times a month; 5 = Once a week; 6 = More than once/week", "1 = Rarely or never; 2 = A few times a month; 3 = Once a week; 4 = Two or more times/week; 5 = Daily; 6 = More than once a day", rep("1 = Definitely not true; 2 = Tends not to be true; 3 = Unsure; 4 = Tends to be true; 5 = Definitely true of me", 3),
-  rep(c("0 = not at all; 1; 2; 3; 4 = extremely"), 5)
-)
-
-dic <- data.frame(
-  item_name = names(dat),
-  item_label = c(item_names, "gender", "age"),
-  scale = c(rep(c("rel", "sui"), each = 5), rep("misc",2)),
-  scale_label = c(rep(c("Religious beliefs", "Suicide tendency"), each = 5), rep("Miscellaneous",2)),
-  values = c(rep("1:6", 5), rep("0:4", 5), "'m', 'f', 'ka'", "5:11"),
-  value_labels = c(value_labels, "m = male; f = female; d = diverse", ""),
-  type = c(rep("integer", n_var), "factor", "integer"),
-  weight = 1,
-  missing = c(rep("-999", n_var), "", "-999")
-)
-
-
-## ----echo=FALSE---------------------------------------------------------------
-dic %>% select(1:2) %>% kable(caption = "Dictionary file")#rmarkdown::paged_table()
-
-## ----include = FALSE----------------------------------------------------------
-dic_file <- dic[,1:2]
+dic_file <- ex_scaledic_dic[, 1:2]
+dic_file %>% kable(caption = "Dictionary file")
 
 ## -----------------------------------------------------------------------------
-dat_dic <- apply_dic(dat, dic_file)
-dat_dic <- check_values(dat_dic, report = TRUE)
-replace_missing(dat_dic)
-
-## ----include = FALSE----------------------------------------------------------
-dic_file <- dic[,1:2]
-
-## -----------------------------------------------------------------------------
-dat_dic <- apply_dic(dat, dic_file)
-
-## ----include = FALSE----------------------------------------------------------
-dic_file <- dic %>% select(1,2,"weight", "type")
+dat_dic <- apply_dic(ex_scaledic_data, dic_file)
 
 ## ----echo=FALSE---------------------------------------------------------------
-dic_file %>% kable(caption = "Dictionary file")#rmarkdown::paged_table()
+dic_file <- ex_scaledic_dic %>% select(1,2, type)
+dic_file %>% kable(caption = "Dictionary file")
+
+## -----------------------------------------------------------------------------
+dat_dic <- apply_dic(ex_scaledic_data, dic_file)
+
+## ----echo=FALSE---------------------------------------------------------------
+dic_file <- ex_scaledic_dic %>% select(1,2, weight, type)
+dic_file %>% kable(caption = "Dictionary file")
+
+## -----------------------------------------------------------------------------
+dat_dic <- apply_dic(ex_scaledic_data, dic_file)
+
+## ----echo=FALSE---------------------------------------------------------------
+dic_file <- ex_scaledic_dic %>% select(1,2, "values", "weight", "type")
+dic_file %>% kable(caption = "Dictionary file")
+
+## -----------------------------------------------------------------------------
+dat_dic <- apply_dic(ex_scaledic_data, dic_file)
+
+## -----------------------------------------------------------------------------
+dat_dic <- check_values(dat_dic, report = TRUE, replace = NA) 
+
+## -----------------------------------------------------------------------------
+dat_dic %>% kable(caption = "data frame with replaced invalid values")
+
+## ----echo=FALSE---------------------------------------------------------------
+dic_file <- ex_scaledic_dic %>% select(1,2, "values", "value_labels", "weight", "type")
+dic_file %>% kable(caption = "Dictionary file")
+
+## -----------------------------------------------------------------------------
+dat_dic <- apply_dic(ex_scaledic_data, dic_file) %>% check_values(replace = NA)
+
+## -----------------------------------------------------------------------------
+dat_dic$rel_1
+
+## -----------------------------------------------------------------------------
+dat_dic$sui_1
+
+## -----------------------------------------------------------------------------
+dat_dic$gender
+
+## -----------------------------------------------------------------------------
+dat_dic$age
+
+## ----echo=FALSE---------------------------------------------------------------
+dic_file <- ex_scaledic_dic %>% select(1,2, values, value_labels, weight, type, missing)
+dic_file %>% kable(caption = "Dictionary file")
+
+## -----------------------------------------------------------------------------
+dat_dic <- apply_dic(ex_scaledic_data, dic_file)
+
+## -----------------------------------------------------------------------------
+dat_dic <- apply_dic(ex_scaledic_data, dic_file, check_values = TRUE)
+
+## -----------------------------------------------------------------------------
+dat_dic %>%
+  slice(9:20) %>% 
+  kable(caption = "Extract from the data frame with replaced missing values and checked values")
+
+## ----echo=FALSE---------------------------------------------------------------
+dic_file <- ex_scaledic_dic
+dic_file %>% kable(caption = "Dictionary file")
+
+## -----------------------------------------------------------------------------
+dat_dic <- apply_dic(ex_scaledic_data, dic_file, check_values = TRUE)
+
+## -----------------------------------------------------------------------------
+dat_dic %>% 
+  select_items(scale == "rel") %>% 
+  descriptives()
+
+## -----------------------------------------------------------------------------
+dat_dic %>% 
+  select_items(scale == "rel") %>% 
+  rename_items() %>%
+  descriptives() %>%
+  kable()
 
--- vignettes/scaledic.R
+++ vignettes/scaledic.R
@@ -12,37 +12,36 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
+## ----dic_example, echo=FALSE--------------------------------------------------
+dic_itrf  |>  
+  select(item_name, item_label, scale, scale_label, subscale, subscale_label, values, value_labels, missing, type) |> 
+  slice(2:4)  |> 
+  kable()
+
 ## ----tab_dic_param, echo = FALSE----------------------------------------------
 out <- tribble(
   ~Parameter, ~Meaning, ~Example,
   "item_name", "A short item name", "itrf_1",
-  "scale", "Abreviation of the scale the item belongs to", "irtf",
-  "subscale", "Abrevation of the sub scale", "int",
-  "subscale_2", "Abrevation of the second order sub scale", "shy",
-  "scale_label", "Name of the scale", "Integrated Teacher Report Form",
-  "subscale_label", "Name of the sub scale", "internalizing problems",
-  "subscale_2_label", "Name of the second order sub scale", "shyness",
   "item_label", "Full text of the item", "Vermeidet die Teilnahme an Diskussionen im Unterricht",
+  #"scale", "Abreviation of the scale the item belongs to", "irtf",
+  #"scale_label", "Name of the scale", "Integrated Teacher Report Form",
+  #"subscale", "Abrevation of the sub scale", "int",
+  #"subscale_label", "Name of the sub scale", "internalizing problems",
   "values", "Valid response values in an R manner", "1:5 (for integers 1 to 5) 1,2,3 (for integers 1, 2, 3)",
   "value_labels", "Labels for each response value", "0 = nicht; 1 = leicht; 2 = mäßig; 3 = stark",
   "missing", "Missing values", "-888, -999",
   "type", "Data type (factor, integer, float, real)", "integer",
   "weight", "Reversion of item and its weight", "1 (positive), -1 (reverse), 1.5 (positive, weights 1.5 times)",
-  "source", "Reference", "Casale et al. (2017)",
-  "note", "Further notes", "Item has low discrimination"
 )
-kable(out, caption = "Columns of a dictionary file")
+kable(out, caption = "Basic columns of a dictionary file")
 
-
-## ----dic_example--------------------------------------------------------------
-dic_itrf %>% slice(2:4) %>% kable()
 
 ## ----apply_dic----------------------------------------------------------------
 # Here we use the example dataset "dat_itrf" and the example dic file "dic_itrf"
 dat <- apply_dic(dat_itrf, dic_itrf)
 
 ## ----list_scales--------------------------------------------------------------
-list_scales(dat, labels = TRUE) %>% kable()
+list_scales(dat, paste0(c("scale", "subscale", "subscale_2"), "_label")) %>% kable()
 
 ## ----check_values-------------------------------------------------------------
 dat <- check_values(dat, replace = NA)
@@ -109,7 +108,7 @@ dat %>%
   descriptives(round = 1)
 
 ## -----------------------------------------------------------------------------
-ex_normtable_int %>% kable()
+ex_normtable_int %>% slice(1:10) %>% kable()
 
 ## -----------------------------------------------------------------------------
 dat$raw_int <- score_scale(dat, subscale == "Int", sum = TRUE, max_na = 0)
