e1a927f41efb4f73125053e144fd208781660a2a
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2025-04-13 08:21:29 +0200

    renamed

--- tests/testthat/test-remove_dic.R
+++ tests/testthat/test-remove_dic.R
@@ -1,7 +1,7 @@
 test_that("remove_dic", {
 
-  dat <- remove_dic(ex_itrf)
-  dat_2 <- remove_dic(ex_itrf, remove_attributes = TRUE)
+  dat <- remove_dic_class(ex_itrf)
+  dat_2 <- remove_dic_class(ex_itrf, remove_attributes = TRUE)
 
   x <- unlist(lapply(dat_2, function(x) attr(x, opt("dic"))))
   expect_equal(any(unlist(lapply(dat, class) == "dic")), FALSE)
