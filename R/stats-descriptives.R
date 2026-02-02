6170e71d4a8516648fd11268b2068765e122a1f2
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2026-02-02 08:52:57 +0100

    renamed file

---
+++ R/stats-descriptives.R
@@ -0,0 +1,50 @@
+#' Table with descriptive statistics
+#'
+#' Computes descriptive statistics for all numeric variables in a data frame.
+#'
+#' @param data A data frame
+#' @param round Digits for round function
+#' @param labels Deprecated. Use [rename_items()] instead.
+#'
+#' @return A data frame with descriptive statistics
+#' @examples
+#' descriptives(ex_itrf)
+#' @export
+descriptives <- function(data, round = 2, labels = FALSE) {
+
+  init_messages(); on.exit(print_messages())
+
+  .filter <- sapply(data, is.numeric)
+
+  if (any(!.filter)) {
+    add_message(
+      "Some variables are not numeric and dropped from the analysis: ",
+      paste0(names(.filter)[!.filter], collapse = ", ")
+    )
+  }
+
+  data <- data[, .filter]
+
+  out <- apply(data, 2, function(x)
+    c(
+      valid = sum(!is.na(x)),
+      missing  = sum(is.na(x)),
+      mean = mean(x, na.rm = TRUE),
+      sd = sd(x, na.rm = TRUE),
+      min = min(x, na.rm = TRUE),
+      max = max(x, na.rm = TRUE),
+      range = max(x, na.rm = TRUE) - min(x, na.rm = TRUE),
+      median = median(x, na.rm = TRUE),
+      mad = mad(x, na.rm = TRUE)
+    ))
+
+  out <- t(out)
+  out <- round(out, round)
+  out <- data.frame(out)
+  out <- cbind(name = rownames(out), out)
+  rownames(out) <- NULL
+  if (labels) add_message("Deprecated. Use rename_items().")
+
+  out
+}
+
---
+++ R/stats-exploratory_fa.R
@@ -0,0 +1,44 @@
+#' Exploratory factor analysis based on the psych::fa function
+#'
+#' Creates a data frame with factor loadings and variance explained.
+#'
+#' @param ... Arguments passed to the psych::fa function.
+#' @param factor_names A character vector with names for the resulting factors.
+#'   If not provided, default names are chosen.
+#' @param sort If TRUE, loadings are sorted.
+#' @param cut Loadings below cut will be omitted in the resulting data frame.
+#' @param round Number of digits to round loadings (based on the base::round
+#'   function).
+#' @seealso \code{\link[psych]{fa}}
+#' @author Jürgen Wilbert
+#' @return A data.frame.
+#' @examples
+#' ## Without factor names
+#' ex_itrf  |>
+#'   select_items(subscale %in% c('Int', 'Ext'))  |>
+#'   exploratory_fa(nfactors = 2)
+#' ## With factor names
+#' ex_itrf  |>
+#'   select_items(subscale %in% c('Int', 'Ext'))  |>
+#'   exploratory_fa(nfactors = 2, factor_names = c("Internalizing", "Externalizing"))
+#' @export
+
+exploratory_fa <- function(...,
+                           factor_names = NULL,
+                           sort = TRUE,
+                           cut = 0.2,
+                           round = 2) {
+  args <- list(...)
+  out <- do.call(psych::fa, args)
+  var_exp <- out$Vaccounted
+  if(sort) out <- psych::fa.sort(out)
+  out <- loadings(out)
+  out <- unclass(out)
+  out <- round(out, round)
+  out[abs(out) < cut] <- ""
+  out <- as.data.frame(out)
+  if (!is.null(factor_names)) names(out) <- factor_names[1:ncol(names)]
+  out <- rbind(out, round(var_exp, round))
+  out
+}
+
